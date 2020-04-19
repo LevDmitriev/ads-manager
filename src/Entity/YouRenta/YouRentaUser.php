9e5230045a9c7c81dc6a69ae44d9baf3ec1d3414
Lev Dmitriev <Lev.Dmitrievv@yandex.ru>
2020-04-19 23:23:16 +0400

    master Добавил модель типов недвижимости yourenta

--- src/Entity/YouRenta/YouRentaUser.php
+++ src/Entity/YouRenta/YouRentaUser.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Entity;
+namespace App\Entity\YouRenta;
 
 use Doctrine\ORM\Mapping as ORM;
 
--- src/Repository/YouRenta/YouRentaUserRepository.php
+++ src/Repository/YouRenta/YouRentaUserRepository.php
@@ -1,8 +1,8 @@
 <?php
 
-namespace App\Repository;
+namespace App\Repository\YouRenta;
 
-use App\Entity\YouRentaUser;
+use App\Entity\YouRenta\YouRentaUser;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
 
