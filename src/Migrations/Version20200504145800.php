5ab2190d006843d98c83306b5a65323bed7932f0
Lev Dmitriev <Lev.Dmitrievv@yandex.ru>
2020-05-04 15:01:38 +0400

    master Заполнил таблицу городов yourenta

---
+++ src/Migrations/Version20200504145800.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use App\Entity\YouRenta\YouRentaCity;
+use App\Entity\YouRenta\YouRentaObjectType;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerAwareTrait;
+
+/**
+ * Миграция добавления типов объектов
+ */
+final class Version20200504145800 extends AbstractMigration implements ContainerAwareInterface
+{
+   use ContainerAwareTrait;
+
+    public function getDescription() : string
+    {
+        return 'Добавить города yourenta';
+    }
+
+    public function up(Schema $schema) : void
+    {
+        $object = new YouRentaCity();
+        $object->setName('Тольятти');
+        $entityManager = $this->container->get('doctrine.orm.default_entity_manager');
+        $entityManager->persist($object);
+        $entityManager->flush();
+    }
+
+    public function down(Schema $schema) : void
+    {
+        $entityManager = $this->container->get('doctrine.orm.default_entity_manager');
+        $repository = $entityManager->getRepository(YouRentaCity::class);
+        $entityManager->remove($repository->findOneBy(['name' => 'Тольятти']));
+        $entityManager->flush();
+    }
+
+}
